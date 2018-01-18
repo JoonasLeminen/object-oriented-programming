03988fef0765615e417be93e94a180824a070e20
joonasleminen <joonas.leminen@outlook.com>
2018-01-18 10:56:23 +0200

    Työntekijä luokka tehty

---
+++ object-method/TaskEmployee/TaskEmployee/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskEmployee
+{
+    class Employee
+    {
+        public string Name;
+        public string Id;
+        public string Position;
+        public int Salary;
+
+        public Employee()
+        {
+            Name = "Työntekijän nimi";
+            Id = "Työntekijän ID";
+            Position = "Työntekijän asema";
+            Salary = 0;
+        }
+
+        public Employee(string name, string id, string position, int salary)
+        {
+            Name = name;
+            Id = id;
+            Position = position;
+            Salary = salary;
+        }
+
+        public void PrintEmployeeInfo()
+        {
+            Console.WriteLine($"Työntekijän nimi: {Name}\n" +
+                              $"Työntekijän ID: {Id}\n" +
+                              $"Työntekijän asema: {Position}\n" +
+                              $"Työntekijän palkka: {Salary:C}\n");
+        }
+
+        public void CompareSalary(Employee employee)
+        {
+            if (Salary > employee.Salary)
+            {
+                Console.WriteLine($"{Name} palkka on suurempi kuin {employee.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"{employee.Name} palkka on suurempi kuin {Name}");
+            }
+        }
+
+    }
+}
---
+++ object-method/TaskEmployee/TaskEmployee/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskEmployee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tehtävä: Työntekijä luokka");
+            Employee Employee1 = new Employee("Matti Meikäläinen", "1337", "Toimitusjohtaja", 3000);
+            Employee Employee2 = new Employee("Maisa Meikäläinen", "1336", "Markkinapäällikkö", 2500);
+            Employee Employee3 = new Employee();
+            Employee1.PrintEmployeeInfo();
+            Employee2.PrintEmployeeInfo();
+            Console.WriteLine();
+
+            Employee1.CompareSalary(Employee2);
+            Console.WriteLine();
+
+            Employee[] employees = new Employee[3];
+            employees[0] = new Employee("Matti Meikäläinen", "1337", "Toimitusjohtaja", 3000);
+            employees[1] = new Employee("Maisa Meikäläinen", "1336", "Markkinapäällikkö", 2500);
+            employees[2] = new Employee("Mikko Meikäläinen", "1338", "Varatoimitusjohtaja", 2400);
+
+            for (int i = 0; i < 3; i++)
+            {
+                employees[i].PrintEmployeeInfo();
+            }
+            employees[1].CompareSalary(employees[2]);
+
+            Console.ReadKey();
+        }
+    }
+}
