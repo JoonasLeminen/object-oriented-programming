a045486074f650b73c129d127d85d9e30c338dc0
Joonas Leminen <joonas.leminen@outlook.com>
2018-02-08 08:04:14 +0200

    TaskLiterature kesken

---
+++ object-method/TaskLiterature/TaskLiteratute/Book.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TaskLiterature
+{
+    class Book
+    {
+        public string Name;
+        public string Author;
+        public string Publisher;
+        public int Price;
+        static string ThemeName;
+
+        public Book()
+        {
+            Name = null;
+            Author = null;
+            Publisher = null;
+            Price = 0;
+            ThemeName = null;
+        }
+
+        public Book(string name, string author, string publisher, int price, string themeName)
+        {
+            name = Name;
+            author = Author;
+            publisher = Publisher;
+            price = Price;
+            themeName = ThemeName;
+        }
+
+        public void GetBookInfo(string bookName)
+        {
+            if (Name == bookName)
+            {
+                Console.WriteLine($"Kirjan nimi: {Name}/n" +
+                                  $"Kirjan tekijä: {Author}/n" +
+                                  $"Kirjan julkaisija: {Publisher}/n" +
+                                  $"Kirjan hinta: {Price}:C/n" +
+                                  $"Kirjan teema: {ThemeName}");
+            }
+            else
+            {
+                Console.WriteLine($"Kirjaa nimeltä {bookName} ei löytynyt.");
+            }
+        }
+        public void ChangeTheme(Book book)
+        {
+            ThemeName = "Fantasia";
+        }
+    }
+}
---
+++ object-method/TaskLiterature/TaskLiteratute/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace TaskLiterature
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            Console.WriteLine("Tehtävä: Kirja-luokka");
+            Book book = new Book("Moukanpeli", "Jussi Vares", "Otava", 30, "Dekkari");
+            Book book2 = new Book("Valkovenäläinen", "Jussi Vares", "Otava", 29, "Dekkari");
+
+            book.GetBookInfo("Moukanpeli");
+            book.GetBookInfo("6/12");
+            book.ChangeTheme(book);
+            book.GetBookInfo("Moukanpeli");
+            book2.GetBookInfo("Valkovenäläinen");
+
+
+            Console.ReadKey();
+        }
+    }
+}
